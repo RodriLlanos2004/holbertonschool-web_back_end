f3f6a184ed5ebb84930fce82c34c5f5982f1b7de
RodriLlanos2004 <rodrigollanos04@gmail.com>
2026-02-03 16:07:20 -0400

    Delete NoSQL directory

--- NoSQL/11-schools_by_topic.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Return list of schools having a specific topic"""
-
-
-def schools_by_topic(mongo_collection, topic):
-    """Return list of documents having topic in topics"""
-    return list(mongo_collection.find({"topics": topic}))
--- NoSQL/8-all.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""List all documents in a collection"""
-
-
-def list_all(mongo_collection):
-    """Return list of documents in mongo_collection"""
-    return list(mongo_collection.find())
